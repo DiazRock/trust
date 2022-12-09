dfaadb3c635dbdd3a0fe044d0f0f9f547e0e6915
DiazRock <corolariodiaz@gmail.com>
2022-12-09 17:53:07 -0500

    Added a pattern matching

--- src/main.rs
+++ src/main.rs
@@ -6,17 +6,26 @@ fn main() -> io::Result<()> {
     let mut buf = [0u8;1500];
     loop {
         let nbytes = nic.recv(&mut buf[..])?;
-        let flags = u16::from_be_bytes([buf[0], buf[1]]);
-        let proto = u16::from_be_bytes([buf[2], buf[3]]);
-        if proto != 0x0800 {
+        let eth_flags = u16::from_be_bytes([buf[0], buf[1]]);
+        let eth_proto = u16::from_be_bytes([buf[2], buf[3]]);
+        if eth_proto != 0x0800 {
             continue;
         }
-        eprintln!("read {} bytes (flags: {:x}, proto: {:x}): {:x?}", 
-        nbytes - 4,
-        flags,
-        proto,
-        &buf[4..nbytes]);
+
+        match etherparse::Ipv4HeaderSlice::from_slice(&buf[4..nbytes]){
+            Ok(p) => {
+                let src = p.source_addr();
+                let dst = p.destination_addr();
+                let proto = p.protocol();
+                eprintln!("{} => {} => {}b of protocol {}", 
+                src,
+                dst,
+                proto,
+                p);
+            }
+            Err(e) => {
+                eprintln!("ignoring weird packet {:?}", e);
+            }
+        }
     }
-    
-    Ok(())
 }
