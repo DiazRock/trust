6fcdd046ac9f80466d5c18b09a9bd1286bbfad01
DiazRock <corolariodiaz@gmail.com>
2022-12-12 09:16:34 -0500

    Using tcp structs for prompt tcp state

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,18 @@
 extern crate tun_tap;
 use std::io;
+use std::collections::HashMap;
+use std::net::Ipv4Addr;
+
+mod tcp;
+
+#[derive(Clone, Copy, Debug, Hash, Eq, PartialEq)]
+struct Quad {
+    src: (Ipv4Addr, u16),
+    dst: (Ipv4Addr, u16),
+}
 
 fn main() -> io::Result<()> {
+    let mut connections: HashMap<Quad, tcp::State> = Default::default();
     let nic = tun_tap::Iface::new("tun0", tun_tap::Mode::Tun)?;
     let mut buf = [0u8;1500];
     loop {
@@ -9,19 +20,32 @@ fn main() -> io::Result<()> {
         let eth_flags = u16::from_be_bytes([buf[0], buf[1]]);
         let eth_proto = u16::from_be_bytes([buf[2], buf[3]]);
         if eth_proto != 0x0800 {
+            // not ipv4
             continue;
         }
 
         match etherparse::Ipv4HeaderSlice::from_slice(&buf[4..nbytes]){
-            Ok(p) => {
-                let src = p.source_addr();
-                let dst = p.destination_addr();
-                let proto = p.protocol();
-                eprintln!("{} => {} => {}b of protocol {}", 
-                src,
-                dst,
-                proto,
-                p);
+            Ok(iph) => {
+                let src = iph.source_addr();
+                let dst = iph.destination_addr();
+                let proto = iph.protocol();
+                if proto != 0x06 {
+                    // not tcp
+                    continue;
+                }
+                match etherparse::TcpHeaderSlice::from_slice(&buf[4+iph.slice().len()..]) {
+                    Ok(tcph) => {
+                        let datai = 4 + iph.slice().len() + tcph.slice().len();
+                        connections.entry(Quad{
+                            src: (src, tcph.source_port()),
+                            dst: (dst, tcph.destination_port())
+                        }).or_default().on_packet(iph, tcph, &buf[datai..]);
+                    },
+                    Err(e) => {
+                        eprintln!("ignoring weird tcp packet {:?}", e);
+                    }
+                }
+
             }
             Err(e) => {
                 eprintln!("ignoring weird packet {:?}", e);
